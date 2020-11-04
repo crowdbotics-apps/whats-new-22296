4d2c08506c0ad66438694e61887497a565a805f8
Crowdbotics <team@crowdbotics.com>
2020-11-04 16:23:03 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed2167534', human_name: 'Feed 2', access_route: 'Feed2167534'},
 { name: 'BlankScreen0167532', human_name: 'Blank Screen 0', access_route: 'BlankScreen0167532'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Feed2167534Navigator from '../features/Feed2167534/navigator';
 import BlankScreen0167532Navigator from '../features/BlankScreen0167532/navigator';
 
 /**
@@ -14,6 +15,7 @@ import BlankScreen0167532Navigator from '../features/BlankScreen0167532/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Feed2167534: { screen: Feed2167534Navigator },
 BlankScreen0167532: { screen: BlankScreen0167532Navigator },
 
     /** new navigators can be added here */
